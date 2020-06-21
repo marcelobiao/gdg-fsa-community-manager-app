d51b034fab5395430ae0354b6d8303eaaa1fd334
Marcelo Biao <marcelobiao2@gmail.com>
2020-06-21 20:32:21 -0300

    Criando model de Event

---
+++ app/Event.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Event extends Model
+{
+    use SoftDeletes;
+    public $timestamps = true;
+
+    protected $fillable = [
+        'name',
+        'date',
+        'place',
+        'city'
+    ];
+
+    protected $rules = [
+        'name' => ['required', 'max:255'],
+        'date' => ['required', 'date'],
+        'place' => ['required', 'max:255'],
+        'city' => ['required', 'max:255']
+    ];
+}
