e8c323cbe39ac0e9daadec292c8cb0035859bfcc
Marcelo Biao <marcelobiao2@gmail.com>
2020-06-21 20:09:32 -0300

    Criando migration de Event

---
+++ database/migrations/2020_06_21_225637_create_events_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable(false);
+            $table->timestamp('date')->nullable(false);
+            $table->string('place')->nullable(false);
+            $table->string('city')->nullable(false);
+            $table->timestamps();
+            $table->softDeletes('deleted_at', 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
