2c46443c804fc5d5e8d33069d10738a91eed0130
Marcelo Biao <marcelobiao2@gmail.com>
2020-07-01 14:44:39 -0300

    Implementando migration de people_event

---
+++ database/migrations/2020_06_22_190932_create_people_event_table.php
@@ -0,0 +1,64 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePeopleEventTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people_event', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('event_id');
+            $table->foreign('event_id')
+                ->references('id')
+                ->on('events')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->unsignedBigInteger('people_id');
+            $table->foreign('people_id')
+                ->references('id')
+                ->on('people')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+                $table->integer('registration_order');
+            $table->string('ticket_hash');
+            $table->string('ticket_type');
+            $table->decimal('ticket_value');
+            $table->timestamp('purchase_date');
+            $table->string('order_hash');
+            $table->enum('payment_status', ['Aprovado', 'Não pago', 'Cancelado']);
+            $table->boolean('check_in')->default(false);
+            $table->timestamp('check_in_date');
+            $table->string('discount_code');
+            $table->enum('payment_method', ['grátis', 'cartão de crédito', 'boleto bancário', 'adicionado manualmente']);
+
+            $table->unsignedBigInteger('import_id');
+            $table->foreign('import_id')
+                ->references('id')
+                ->on('imports')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('people_event');
+    }
+}
