d58ae15fc50a6d5c56ec4ebb4c355d8cd2f16da8
Marcelo Biao <marcelobiao2@gmail.com>
2020-07-01 14:25:39 -0300

    Implementando migration de people

---
+++ database/migrations/2020_06_22_183751_create_people_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePeopleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->id();
+            $table->string('email')->unique();
+            $table->string('name');
+            $table->string('about')->nullable(true);
+            $table->unsignedBigInteger('import_id');
+            $table->foreign('import_id')
+                ->references('id')->on('imports')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('people');
+    }
+}
