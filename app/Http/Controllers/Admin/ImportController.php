118f0d286fb794e0fc61925e7c8cbbe19a9657c6
Marcelo Biao <marcelobiao2@gmail.com>
2020-07-03 15:08:43 -0300

    Implementando controller de import

---
+++ app/Http/Controllers/Admin/ImportController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpFoundation\Response;
+use Exception;
+use App\Import;
+use App\Exceptions\ImportException;
+
+class ImportController extends Controller
+{
+    private $model;
+
+    public function __construct(Import $import){
+        $this->model = $import;
+    }
+
+    public function index(){
+        try{
+            $import = $this->model->all();
+            return response()->json($import);
+        }catch(ImportException $ex){
+            return response()->json(['error' => $ex->getMessage()], Response::HTTP_BAD_REQUEST);
+        }catch(Exception $ex){
+            return response()->json(['error' => $ex->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function show($id){
+        try{
+            $import = $this->model->find($id);
+            throw_if(empty($import), new ImportException(ImportException::IMPORT_NOT_FOUND));
+
+            return response()->json($import);
+        }catch(ImportException $ex){
+            return response()->json(['error' => $ex->getMessage()], Response::HTTP_BAD_REQUEST);
+        }catch(Exception $ex){
+            return response()->json(['error' => $ex->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function store(Request $request){
+        try{
+            $data = $request->all();
+            $validator = Validator::make($data, $this->model->rules());
+
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), Response::HTTP_BAD_REQUEST);
+            }
+
+            //TODO: Importar Registros do arquivo
+            $import = $this->model->create($data);
+
+            return  response()->json($import);
+        }catch(ImportException $ex){
+            return response()->json(['error' => $ex->getMessage()], Response::HTTP_BAD_REQUEST);
+        }catch(Exception $ex){
+            return response()->json(['error' => $ex->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function update($id, Request $request){
+        try{
+            $import = $this->model->find($id);
+            throw_if(empty($import), new ImportException(ImportException::IMPORT_NOT_FOUND));
+
+            $data = $request->all();
+            $import->fill($data);
+            $validator = Validator::make($import->toArray(), $import->rules());
+
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), Response::HTTP_BAD_REQUEST);
+            }
+
+            $import->save();
+
+            return response()->json($import);
+        }catch(ImportException $ex){
+            return response()->json(['error' => $ex->getMessage()], Response::HTTP_BAD_REQUEST);
+        }catch(Exception $ex){
+            return response()->json(['error' => $ex->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function destroy($id){
+        try{
+            $import = $this->model->find($id);
+            throw_if(empty($import), new ImportException(ImportException::IMPORT_NOT_FOUND));
+            $import->delete();
+            return response()->json('People removed');
+        }catch(ImportException $ex){
+            return response()->json(['error' => $ex->getMessage()], Response::HTTP_BAD_REQUEST);
+        }catch(Exception $ex){
+            return response()->json(['error' => $ex->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
