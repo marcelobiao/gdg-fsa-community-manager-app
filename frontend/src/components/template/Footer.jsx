330ecf99bf7fcd7f84daa89882d73f989f667253
marcelobiao <marcelobiao2@gmail.com>
2020-11-14 09:32:21 -0300

    Removendo projetos antigos de frontend

--- frontend/src/components/template/Footer.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import './Footer.css'
-
-export default props =>
-    <footer className="footer">
-        Desenvolvido com <i className="fa fa-heart text-danger"></i>por <strong>Cod <span className="text-danger">3</span>r</strong>
-    </footer>
