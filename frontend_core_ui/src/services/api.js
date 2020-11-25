53c36ba82509c9c354f8e69bb56965957673f283
marcelobiao <marcelobiao2@gmail.com>
2020-11-24 21:29:41 -0300

    Ajustando service api para utilizar o access_token obtido durante o login

--- frontend_core_ui/src/services/api.js
+++ frontend_core_ui/src/services/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
-
+const access_token = localStorage.getItem('access_token');
 const api = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
-    /* headers: {
-        'Access-Control-Allow-Origin': true,
-      }, */
+    headers: {
+        Authorization: 'bearer ' + access_token,
+      },
 });
 
 export default api;
