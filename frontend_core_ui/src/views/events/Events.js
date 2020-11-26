76007cc89c1f7cfbebf4155563a1f22e232cc67f
Roquelson <roquelsonferreira@assistevida.com>
2020-11-26 12:18:39 -0300

    Modal para cadastro de evento e funcionalidade de cadastro implementados

--- frontend_core_ui/src/views/events/Events.js
+++ frontend_core_ui/src/views/events/Events.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 import {
   CCard,
@@ -7,41 +7,74 @@ import {
   CButton,
   CRow,
   CCol,
-} from '@coreui/react'
+  CModal,
+  CModalBody,
+  CModalFooter,
+  CModalHeader,
+  CModalTitle,
+  CForm,
+  CFormGroup,
+  CFormText,
+  CLabel,
+  CTextarea,
+  CInput,
+} from "@coreui/react";
 
-import api from '../../services/api'
+import api from "../../services/api";
 
-const fields = ['id', 'name', 'date', 'place', 'city','actions'];
+const fields = ["id", "name", "date", "place", "city", "actions"];
 
-
-const editEvent = (index) => {
-  
-}
-
+const editEvent = (index) => {};
 
 const Events = () => {
-
   const [events, setEvents] = useState([]);
+  const [name, setName] = useState("");
+  const [date, setDate] = useState("");
+  const [place, setPlace] = useState("");
+  const [city, setcity] = useState("");
+  const [primary, setPrimary] = useState(false);
+  const [requisition, setRequisition] = useState(false);
+
+  async function handleCreateEvent(e) {
+    e.preventDefault();
+
+    const event = { name, date, place, city };
+
+    try {
+      await api.post("/admin/events/", event);
+      setRequisition(!requisition);
+      setPrimary(!primary);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   const deleteEvent = (index) => {
-    api.delete(`/admin/events/${index}`).then(response=>{
-     setEvents(events.filter((event)=>{
-       return event.id!==index;
-     }))
-    }).catch(response=>{
-      console.log(response);
-    })
-   }
+    api
+      .delete(`/admin/events/${index}`)
+      .then(() => {
+        setEvents(
+          events.filter((event) => {
+            return event.id !== index;
+          })
+        );
+      })
+      .catch((response) => {
+        console.log(response);
+      });
+  };
 
   useEffect(() => {
-    api.get('/admin/events')
-      .then(response => {
+    api
+      .get("/admin/events")
+      .then((response) => {
         console.log(response);
         setEvents(response.data);
-      }).catch(response => {
-        console.log(response);
       })
-  }, []);
+      .catch((response) => {
+        console.log(response);
+      });
+  }, [requisition]);
 
   return (
     <>
@@ -56,18 +89,18 @@ const Events = () => {
             size="lg"
             itemsPerPage={10}
             pagination
-            scopedSlots ={{
-              'actions':
-              (item, index)=>{
+            scopedSlots={{
+              actions: (item, index) => {
                 return (
-                  <td  >
+                  <td>
                     <CButton
-                      
                       color="primary"
                       variant="ghost"
                       shape="square"
                       size=""
-                      onClick={()=>{editEvent(item.id)}}
+                      onClick={() => {
+                        editEvent(item.id);
+                      }}
                     >
                       Edit
                     </CButton>
@@ -76,32 +109,116 @@ const Events = () => {
                       variant="ghost"
                       shape="square"
                       size=""
-                      onClick={()=>{deleteEvent(item.id)}}
+                      onClick={() => {
+                        deleteEvent(item.id);
+                      }}
                     >
                       Delete
                     </CButton>
                   </td>
-                  )
+                );
               },
             }}
           />
         </CCardBody>
-                  <CRow className="mx-auto" style={{width:"200px"}}>
-                    <CCol col="12"  >
-                     <CButton
-                      color="success"
-                     
-                      shape="square"
-                      size="sm"
-                      
-                    >
-                      Create Event
-                    </CButton>
-                    </CCol >
-                 </CRow >
+        <CRow className="mx-auto" style={{ width: "200px" }}>
+          <CCol col="12">
+            <CButton
+              color="primary"
+              shape="square"
+              size="sm"
+              onClick={() => setPrimary(!primary)}
+            >
+              Create Event
+            </CButton>
+          </CCol>
+        </CRow>
+        <CModal
+          show={primary}
+          onClose={() => setPrimary(!primary)}
+          color="primary"
+        >
+          <CModalHeader closeButton>
+            <CModalTitle>Create</CModalTitle>
+          </CModalHeader>
+          <CModalBody>
+            <CForm>
+              <CFormGroup row>
+                <CCol md="3">
+                  <CLabel htmlFor="text-input">Name</CLabel>
+                </CCol>
+                <CCol xs="12" md="9">
+                  <CInput
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    type="text"
+                    name="text-input"
+                    placeholder="Text"
+                  />
+                  <CFormText>This is a help text</CFormText>
+                </CCol>
+              </CFormGroup>
+
+              <CFormGroup row>
+                <CCol md="3">
+                  <CLabel htmlFor="date-input">Date</CLabel>
+                </CCol>
+                <CCol xs="12" md="9">
+                  <CInput
+                    value={date}
+                    onChange={(e) => setDate(e.target.value)}
+                    type="date"
+                    name="date-input"
+                    placeholder="date"
+                  />
+                </CCol>
+              </CFormGroup>
+
+              <CFormGroup row>
+                <CCol md="3">
+                  <CLabel htmlFor="text-input">Place</CLabel>
+                </CCol>
+                <CCol xs="12" md="9">
+                  <CInput
+                    value={place}
+                    onChange={(e) => setPlace(e.target.value)}
+                    type="text"
+                    name="text-input"
+                    placeholder="Text"
+                  />
+                  <CFormText>This is a help text</CFormText>
+                </CCol>
+              </CFormGroup>
+
+              <CFormGroup row>
+                <CCol md="3">
+                  <CLabel htmlFor="text-input">City</CLabel>
+                </CCol>
+                <CCol xs="12" md="9">
+                  <CInput
+                    value={city}
+                    onChange={(e) => setcity(e.target.value)}
+                    type="text"
+                    name="text-input"
+                    placeholder="Text"
+                  />
+                  <CFormText>This is a help text</CFormText>
+                </CCol>
+              </CFormGroup>
+            </CForm>
+          </CModalBody>
+          <CModalFooter>
+            <CButton color="primary" onClick={handleCreateEvent}>
+              Create
+            </CButton>
+            <CButton color="secondary" onClick={() => setPrimary(!primary)}>
+              Cancel
+            </CButton>
+          </CModalFooter>
+        </CModal>
       </CCard>
     </>
-  )
-}
+  );
+};
 
-export default Events
+export default Events;
