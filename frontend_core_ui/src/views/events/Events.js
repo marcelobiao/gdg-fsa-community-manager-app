add791997709dc3151f120e1abcd56cd0b6f7761
Roquelson <roquelsonferreira@assistevida.com>
2020-11-26 21:28:36 -0300

    Função Editar evento implementada

--- frontend_core_ui/src/views/events/Events.js
+++ frontend_core_ui/src/views/events/Events.js
@@ -99,6 +99,30 @@ const openEditEventModal = ({ id,name, date, place, city }) =>{
    setSecondary(!secondary);
   };
 
+const openEditEventModal = ({ id,name, date, place, city }) =>{
+  setSecondary(!secondary);
+  var data = date.replace("00:00:00","");
+  data=data.replace(" ","")
+  setId(id);
+  setName(name);
+  setDate(data);
+  setPlace(place);
+  setcity(city);
+}
+
+  const editEvent = async() => {
+     const event = { name, date, place, city };
+   
+   try{
+    await api.put(`/admin/events/${id}`, event);
+    console.log("editado com sucesso");
+    setRequisition(!requisition);
+   }catch(error){
+     console.log("erro")
+   }
+   setSecondary(!secondary);
+  };
+
   useEffect(() => {
     api
       .get("/admin/events")
@@ -249,7 +273,87 @@ const openEditEventModal = ({ id,name, date, place, city }) =>{
             </CButton>
           </CModalFooter>
         </CModal>
-        
+        <CModal
+          show={secondary}
+          onClose={() => setSecondary(!secondary)}
+          color="primary"
+        >
+          <CModalHeader closeButton>
+            <CModalTitle>Edit</CModalTitle>
+          </CModalHeader>
+          <CModalBody>
+            <CForm>
+              <CFormGroup row>
+                <CCol md="3">
+                  <CLabel htmlFor="text-input">Name</CLabel>
+                </CCol>
+                <CCol xs="12" md="9">
+                  <CInput
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    type="text"
+                    name="text-input"
+                    placeholder="Text"
+                  />
+                  <CFormText>This is a help text</CFormText>
+                </CCol>
+              </CFormGroup>
+
+              <CFormGroup row>
+                <CCol md="3">
+                  <CLabel htmlFor="date-input">Date</CLabel>
+                </CCol>
+                <CCol xs="12" md="9">
+                  <CInput
+                    value={date}
+                    onChange={(e) => setDate(e.value)}
+                    type="date"
+                    name="date-input"
+                    placeholder="date"
+                  />
+                </CCol>
+              </CFormGroup>
+
+              <CFormGroup row>
+                <CCol md="3">
+                  <CLabel htmlFor="text-input">Place</CLabel>
+                </CCol>
+                <CCol xs="12" md="9">
+                  <CInput
+                    value={place}
+                    onChange={(e) => setPlace(e.target.value)}
+                    type="text"
+                    name="text-input"
+                    placeholder="Text"
+                  />
+                  <CFormText>This is a help text</CFormText>
+                </CCol>
+              </CFormGroup>
+
+              <CFormGroup row>
+                <CCol md="3">
+                  <CLabel htmlFor="text-input">City</CLabel>
+                </CCol>
+                <CCol xs="12" md="9">
+                  <CInput
+                    value={city}
+                    onChange={(e) => setcity(e.target.value)}
+                    type="text"
+                    name="text-input"
+                    placeholder="Text"
+                  />
+                  <CFormText>This is a help text</CFormText>
+                </CCol>
+              </CFormGroup>
+            </CForm>
+          </CModalBody>
+          <CModalFooter>
+            <CButton color="primary" onClick={editEvent}>Edit</CButton>
+            <CButton color="secondary" onClick={() => setSecondary(!secondary)}>
+              Cancel
+            </CButton>
+          </CModalFooter>
+        </CModal>
       </CCard>
     </>
   );
